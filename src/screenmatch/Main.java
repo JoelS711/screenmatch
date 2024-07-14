01cce39686acafe3b3ca067cd902f40bd81c1ab8
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-07-14 12:36:48 -0500

    Se agregan getters and setters

--- src/screenmatch/Main.java
+++ src/screenmatch/Main.java
@@ -1,13 +1,15 @@
 package screenmatch;
 
+import model.Movie;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Movie encanto = new Movie();
-		encanto.name = "Encanto";
-		encanto.durationTime = 120;
-		encanto.relaseDate = 2022;
+		encanto.setName("Encanto");
+		encanto.setDurationTime(120);
+		encanto.setRelaseDate(2022);
 		encanto.showTechnicalSheet();
 
 	}
