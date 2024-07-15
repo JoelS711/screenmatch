4f04183219f76b987f66a414ca47eca5230d7f19
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-07-15 13:48:00 -0500

    Se modifica logica de ingresar datos

--- src/screenmatch/Main.java
+++ src/screenmatch/Main.java
@@ -1,26 +1,12 @@
 package screenmatch;
 
-import model.Movie;
-import model.Serie;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Movie encanto = new Movie();
-		encanto.setName("Encanto");
-		encanto.setDurationTime(120);
-		encanto.setRelaseDate(2022);
-		encanto.showTechnicalSheet();
-		
-		
-		Serie friends = new Serie();
-		friends.setName("Friends");
-		friends.setRelaseDate(1996);
-		friends.setSeasons(10);
-		friends.setEpisodesPerSeason(22);
-		friends.setDurationInMinutesPerEpisode(30);
-		friends.showTechnicalSheet();
+		Principal principal = new Principal();
+		principal.showTheMenu();
 
 	}
 
--- src/screenmatch/Principal.java
+++ src/screenmatch/Principal.java
@@ -1,5 +1,52 @@
 package screenmatch;
 
+import java.util.Scanner;
+
+import model.Movie;
+
 public class Principal {
+
+	public void showTheMenu() {
+		int option = 0;
+		Scanner keyboard = new Scanner(System.in);
+		while (option != 9) {
+			String menu = """
+					Welcome to Screenmatch
+					1. Register new movie
+					2. Register new serie
+					9. Exit
+					""";
+			System.out.println(menu);
+			option = keyboard.nextInt();
+			keyboard.nextLine();
+
+			switch (option) {
+			case 1:
+				System.out.println("Enter the name of the movie: ");
+				String name = keyboard.nextLine();
+				System.out.println("Enter the relase date: ");
+				int relaseDate = keyboard.nextInt();
+				keyboard.nextLine();
+				System.out.println("Enter the length of the movie in minutes: ");
+				int durationTime = keyboard.nextInt();
+				keyboard.nextLine();
+
+				Movie userMovie = new Movie();
+				userMovie.setName(name);
+				userMovie.setRelaseDate(relaseDate);
+				userMovie.setDurationTime(durationTime);
+				userMovie.showTechnicalSheet();
+				break;
+				
+			case 9:
+				System.out.println("Saliendo del programa");
+				break;
+			default:
+				System.out.println("Invalid option");
+				break;
+
+			}
+		}
+	}
 
 }
