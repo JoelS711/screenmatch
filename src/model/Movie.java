5b41d7df9664e98aceb9c94ad41765c1e3bd8096
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-07-15 12:10:54 -0500

    Se modifica clase Title

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -2,78 +2,15 @@ package model;
 
 public class Movie {
 
-	private String name;
-	private int relaseDate;
-	private double qualification;
-	private String synopsis;
-	private int durationTime;
+	private String director;
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
 	
 	
-	
-	public String getName() {
-		return name;
-	}
-
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public int getRelaseDate() {
-		return relaseDate;
-	}
-
-
-
-	public void setRelaseDate(int relaseDate) {
-		this.relaseDate = relaseDate;
-	}
-
-
-
-	public double getQualification() {
-		return qualification;
-	}
-
-
-
-	public void setQualification(double qualification) {
-		this.qualification = qualification;
-	}
-
-
-
-	public String getSynopsis() {
-		return synopsis;
-	}
-
-
-
-	public void setSynopsis(String synopsis) {
-		this.synopsis = synopsis;
-	}
-
-
-
-	public int getDurationTime() {
-		return durationTime;
-	}
-
-
-
-	public void setDurationTime(int durationTime) {
-		this.durationTime = durationTime;
-	}
-
-
-
-	public void showTechnicalSheet() {
-		System.out.println("***FICHA TECNICA***");
-		System.out.println("Name of the movie: " + name);
-		System.out.println("Relase date: " + relaseDate);
-		System.out.println("Duration time in minutes: " + durationTime);
-	}
 }
