898f410579eb2aff85728aa63e16f1ce88bafe35
Emanuel Gelvez <81266779+JoelS711@users.noreply.github.com>
2024-07-16 19:24:42 -0500

    Update Movie.java

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -11,6 +11,27 @@ public class Movie extends Title{
 	public void setDirector(String director) {
 		this.director = director;
 	}
+
+public static Movie getMovieDetailsFromUser() {
+        System.out.println("Enter the name of the movie: ");
+        String _name = keyboard.nextLine();
+        
+        System.out.println("Enter the release date: ");
+        int _releaseDate = keyboard.nextInt();
+        keyboard.nextLine(); // clear the buffer
+        
+        System.out.println("Enter the length of the movie in minutes: ");
+        int _durationTime = keyboard.nextInt();
+        keyboard.nextLine(); // clear the buffer
+        
+        Movie userMovie = new Movie();
+        userMovie.setName(_name);
+        userMovie.setReleaseDate(_releaseDate);
+        userMovie.setDurationTime(_durationTime);
+        
+        return userMovie;
+    }
+}
 	
 	
 }
