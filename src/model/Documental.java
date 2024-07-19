6cc81a2962fbde85f08b1e1774c1ff3dc1579006
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-07-19 17:47:33 -0500

    Se crea clase Documental

---
+++ src/model/Documental.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Documental {
+
+	private String distributedBy;
+
+	public String getDistributedBy() {
+		return distributedBy;
+	}
+
+	public void setDistributedBy(String distributedBy) {
+		this.distributedBy = distributedBy;
+	}
+	
+	
+}
