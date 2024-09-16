1c0fdb1c5047e4e5267add9be0469bafda26d5a8
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-09-15 20:23:06 -0500

    Update TimeCalculator.class and .java

--- src/calculate/TimeCalculator.java
+++ src/calculate/TimeCalculator.java
@@ -1,5 +1,17 @@
 package calculate;
 
+import model.Title;
+
 public class TimeCalculator {
 
+	private int totalTime;
+	
+	public int getTotalTime() {
+		return this.totalTime;
+	}
+	
+	public void include(Title title) {
+		System.out.println("Adding duration in minutes");
+		this.totalTime += title.getDurationTime();
+	}
 }
