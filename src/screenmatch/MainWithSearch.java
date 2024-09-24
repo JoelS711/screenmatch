3776a858ac7acd1a357bd07bf0dbb569c56342a2
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-09-23 20:23:14 -0500

    Update MainWithSearch

--- src/screenmatch/MainWithSearch.java
+++ src/screenmatch/MainWithSearch.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter de name of the movie: ");
+		var nameMovie = keyboard.nextLine();
+		
+		String url = "http://www.omdbapi.com/?t="+nameMovie+"&apikey=9f0dabfa";
+		
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=9f0dabfa")).build();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 		System.out.println(response.body());
