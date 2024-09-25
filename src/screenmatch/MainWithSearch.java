a7c1b536020bc7fef5b283c1d0fff7b6187b1aee
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-09-24 22:16:34 -0500

    Update MainWithSearch

--- src/screenmatch/MainWithSearch.java
+++ src/screenmatch/MainWithSearch.java
@@ -7,6 +7,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+
+import model.Title;
+
 public class MainWithSearch {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -20,7 +24,12 @@ public class MainWithSearch {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-		System.out.println(response.body());
+		String json = response.body();
+		System.out.println(json);
+		
+		Gson gson = new Gson();
+		Title myTitle = gson.fromJson(json, Title.class);
+		System.out.println(myTitle);
 	}
 
 }
